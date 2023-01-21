e681d6802f3b834fd8f2cc95297b4d19e98c328b
Juan Jacobs <juanmjacobs@gmail.com>
2023-01-20 22:16:33 -0300

    nuevo endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('test')
+  getTest(): string {
+    return this.appService.getTest();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  getTest(): string {
+    return 'Hello Test!';
+  }
   getHello(): string {
     return 'Hello World!';
   }
